90a0841ae9dceb8ee68080ad657113da67100c31
Muhammad-Hakami <engmuhammadhakami@gmail.com>
2020-03-04 02:32:27 +0300

    finished Hamming

--- hamming/hamming.jl
+++ hamming/hamming.jl
@@ -1,3 +1,8 @@
 function distance(s1::AbstractString, s2::AbstractString)
-
+    count=0
+    length(s1)!=length(s2) ? throw(ArgumentError("different strand lengths")) : Nothing
+    for (x,y) in zip(collect(s1),collect(s2))
+        x!=y ? count+=1 : Nothing
+    end
+    return count
 end
